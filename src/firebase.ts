a770c0a836aeef0faa035b476df1ab723f342775
iely chabrouni <ichabrouni@intelligenttech.se>
2025-11-13 12:32:15 +0200

    Update firebase config to use VITE env variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,14 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAYYK2__D_8oh_zl8pkyElZiGLewupss1s",
-  authDomain: "flexpayzcom.firebaseapp.com",
-  databaseURL: "https://flexpayzcom-default-rtdb.firebaseio.com",
-  projectId: "flexpayzcom",
-  storageBucket: "flexpayzcom.firebasestorage.app",
-  messagingSenderId: "556480382433",
-  appId: "1:556480382433:web:0afae9fd02a6c3ce19fea0",
-  measurementId: "G-RCZCF3Y28V"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
